26c8de11f068282815a34a8975e43c1cce22dd5c
FuadNabi <fuad.nebiyev95@mail.ru>
2023-03-27 19:21:10 +0400

    Create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'users#index'
+  authenticated do
+    root to: "categories#index", as: :authenticated_user
+  end
+  root to: "home#index", as: :unauthenticated_user
+
+
   resources :transactions
   resources :categories
   resources :users
