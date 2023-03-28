e0c3015ed1957e1f63a7bb7a7c75b7d93b3eac5b
FuadNabi <fuad.nebiyev95@mail.ru>
2023-03-28 12:10:02 +0400

    Replace transactrion table with entity

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  
   authenticated do
     root to: "categories#index", as: :authenticated_user
   end
   root to: "home#index", as: :unauthenticated_user
-
-
-  resources :transactions
+  
+  
+  resources :entities
   resources :categories
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
